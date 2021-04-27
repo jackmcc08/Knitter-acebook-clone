3467b0d54c45f514f2d8a05e3b5a404b367cc49f
mstrivens <max.strivens@live.co.uk>
2021-04-27 17:05:07 +0100

    Functional issues. currently can press add friend but it doesn't seem to actually do it

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -7,13 +7,20 @@ class FriendsController < ApplicationController
   end
 
   def create
-    @friend = current_user.friends.build(:other_user_id => params[:other_user_id])
+    # @friend = current_user.friends.build(:other_user_id => params[:other_user_id])
+    @friend = Friend.create(:user_id => params[:user_id], :other_user_id => params[:other_user_id])
     if @friend.save
       flash[:notice] = "Friend Added"
-      redirect_to root_url
+      redirect_to posts_url
     else
       flash[:notice] = "Unable to add friend."
-      redirect_to root_url
+      redirect_to posts_url
     end
   end
+
+  # private
+  #
+  # def post_params
+  #   params.require(:friend).permit(:user_id, :other_user_id)
+  # end
 end
