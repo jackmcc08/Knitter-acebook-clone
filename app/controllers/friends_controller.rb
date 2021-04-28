5b038ff99522a2fadeb21dc2a9a0f4291dc079f6
Natalia_Sherchenkova <tataterer@gmail.com>
2021-04-28 13:56:15 +0100

    trying to make friend save

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -8,10 +8,14 @@ class FriendsController < ApplicationController
 
   def create
     # @friend = current_user.friends.build(:other_user_id => params[:other_user_id])
-    @friend = Friend.new(post_params)
+    @friend = Friend.new(:user_id => params[:user_id], :other_user_id => params[:other_user_id])
+    puts 'NEW FRIEND'
     puts @friend
-    puts current_user.friends.build
+    puts 'NEW FRIEND2'
+    puts params[:user_id]
+    puts 'NEW FRIEND3'
     puts params[:other_user_id]
+    puts 'NEW FRIEND4'
     puts current_user.friends
     if @friend.save
       flash[:notice] = "Friend Added"
@@ -24,7 +28,7 @@ class FriendsController < ApplicationController
 
   private
 
-  def post_params
-    params.require(:friend).permit(:user_id, :other_user_id)
-  end
+  # def post_params
+  #   params.require(:friend).permit(:user_id, :other_user_id)
+  # end
 end
