482a0e24289439f7bd562f977522a23da0f2524b
mstrivens <max.strivens@live.co.uk>
2021-04-28 14:52:30 +0100

    Add friend button now adds to the friend database

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -9,14 +9,6 @@ class FriendsController < ApplicationController
   def create
     # @friend = current_user.friends.build(:other_user_id => params[:other_user_id])
     @friend = Friend.new(:user_id => params[:user_id], :other_user_id => params[:other_user_id])
-    puts 'NEW FRIEND'
-    puts @friend
-    puts 'NEW FRIEND2'
-    puts params[:user_id]
-    puts 'NEW FRIEND3'
-    puts params[:other_user_id]
-    puts 'NEW FRIEND4'
-    puts current_user.friends
     if @friend.save
       flash[:notice] = "Friend Added"
       redirect_to posts_url
--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,4 +1,4 @@
 class Friend < ApplicationRecord
   belongs_to :user
-  belongs_to :new_friend, :class_name => 'User'
+  # belongs_to :new_friend, :class_name => 'User'
 end
