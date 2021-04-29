cb2baa08de23d5b861c3fd64d07d40effedbf108
Sha <cspoppuppy@hotmail.com>
2021-04-29 13:15:53 +0100

    added pop up chatroom window, but not working properly yet

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Popup from './Popup';
+import { useState } from 'react';
+
+// const [buttonPopup, setButtonPopup] = useState(false);
 
 class App extends Component {
 	render() {
+		var buttonPopup = false;
 		return (
 			<div>
-				<button>Tearoom</button>
+				<button onClick={() => (buttonPopup = true)}>Tearoom</button>
+				<Popup trigger={buttonPopup}>
+					<h3>Popup</h3>
+				</Popup>
 			</div>
 		);
 	}
--- app/javascript/components/Popup.js
+++ app/javascript/components/Popup.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Popup.css';
 
 function Popup(props) {
 	return props.trigger ? (
 		<div className="popup">
-			<div className="popup inner">
+			<div className="popup-inner">
 				<button className="button">CLOSE</button>
 				{props.children}
 			</div>
@@ -13,4 +14,4 @@ function Popup(props) {
 	);
 }
 
-export default Chat;
+export default Popup;
