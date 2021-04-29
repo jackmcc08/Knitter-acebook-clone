33277fd327122fbf7124b937b633380270601b4e
Natalia_Sherchenkova <tataterer@gmail.com>
2021-04-29 19:31:05 +0100

    list of friends shows on a different page when clicked on an icon next to the comment

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -6,8 +6,13 @@ class FriendsController < ApplicationController
   end
 
   def create
-        @friend = Friend.create(:user_id => params[:user_id], :other_user_id => params[:other_user_id])
-      flash[:notice] = "Friend Added"
-      redirect_to posts_url
+    @friend = Friend.create(:user_id => params[:user_id], :other_user_id => params[:other_user_id])
+    flash[:notice] = "Friend Added"
+    redirect_to posts_url
   end
+
+  def show
+    @friend = User.find(params[:id])
+  end
+
 end
