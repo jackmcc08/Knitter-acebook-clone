776390234e14c101b4683d7693a2bc716bd2cf32
Sha <cspoppuppy@hotmail.com>
2021-04-29 12:03:35 +0100

    setup React in Rails

---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<button>Tearoom</button>
+			</div>
+		);
+	}
+}
+
+export default App;
