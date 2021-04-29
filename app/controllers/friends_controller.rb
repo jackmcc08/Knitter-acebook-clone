b2ad8231a20fab1053026236f52e2f30686e6813
mstrivens <max.strivens@live.co.uk>
2021-04-29 17:15:38 +0100

    Friends controller refactored

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,23 +3,11 @@ class FriendsController < ApplicationController
   end
 
   def new
-    @friend = Friend.new
   end
 
   def create
-    if already_friends?
-      flash[:notice] = "Unable to add friend"
-      redirect_to posts_url
-    else
-      @friend = Friend.create(:user_id => params[:user_id], :other_user_id => params[:other_user_id])
+        @friend = Friend.create(:user_id => params[:user_id], :other_user_id => params[:other_user_id])
       flash[:notice] = "Friend Added"
       redirect_to posts_url
-    end
-  end
-
-  private
-
-  def already_friends?
-    Friend.where(user_id: current_user.id, other_user_id: params[:other_user_id]).exists?
   end
 end
