d11ba4ac5fc095dc377bf8581f886dc5f3d64313
mstrivens <max.strivens@live.co.uk>
2021-04-29 14:45:50 +0100

    Friend is now bidrectional regardless of initiator.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,25 @@
 class User < ApplicationRecord
   has_many :posts
 
-  # has_many :friends
-  # has_many :new_friends, :through => :friends
+  has_many(
+    :a_friends,
+    class_name: "Friend",
+    foreign_key: :user_id,
+    primary_key: :id
+  )
+  has_many(
+    :b_friends,
+    class_name: "Friend",
+    foreign_key: :other_user_id,
+    primary_key: :id
+  )
 
-  has_many :user_friends, :foreign_key => "user_id", :class_name => "Friend"
-  has_many :other_user_friends, :foreign_key => "other_user_id", :class_name => "Friend"
+  has_many :friend_as, through: :b_friends, source: :friend_a
+  has_many :friend_bs, through: :a_friends, source: :friend_b
 
-
-  # has_many :payer_payments, class_name: 'Payment',
-  #                           foreign_key: 'payer_id'
-  # has_many :payee_payments, class_name: 'Payment',
-  #                           foreign_key: 'payee_id'
+  def friends
+    friend_as + friend_bs
+  end
 
   has_secure_password
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_28_140456) do
+ActiveRecord::Schema.define(version: 2021_04_29_114336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "friends", force: :cascade do |t|
-    t.integer "other_user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.bigint "other_user_id"
+    t.index ["other_user_id"], name: "index_friends_on_other_user_id"
     t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
@@ -40,5 +41,6 @@ ActiveRecord::Schema.define(version: 2021_04_28_140456) do
   end
 
   add_foreign_key "friends", "users"
+  add_foreign_key "friends", "users", column: "other_user_id"
   add_foreign_key "posts", "users"
 end
